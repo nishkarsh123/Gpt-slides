1ad0a66888477dcfbf47eb3051f53eed1fb46ee4
Nishkarsh Kumar <nishkarsh.kumar@invenics.com>
2024-05-30 17:42:11 +0530

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase # Import the view you want to test
-from opai.views import testGPTView
-
-class YourTestCase(TestCase):
-    def test_post_request(self):        
-        # Simulate a POST request
-        response = self.client.post('/opai/testgpt/')
-        
-        print (response.content)
